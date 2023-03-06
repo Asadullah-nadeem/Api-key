fed843795c9a50a2f531891311e90eeaa67ca494
Asadullah <88024587+Asadullah-nadeem@users.noreply.github.com>
2023-03-06 18:26:28 +0530

    Update apikey.php

--- apikey.php
+++ apikey.php
@@ -1,5 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
 
 // //api key
 // $apiKey = bin2hex(random_bytes(8)); // generates a 32-character random hexadecimal string
@@ -12,6 +11,7 @@ header("Access-Control-Allow-Origin: *");
 
 // header('X-API-KEY: ' . $apiKey);
 
+// Define a function to generate a random API key
 // Define a function to generate a random API key
 function generateApiKey() {
     $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -43,16 +43,17 @@ function keyExists($key) {
     // For simplicity, we'll assume that the key does not exist
     return false;
 }
-$apiKey = generateApiKey();
-if (isset($_SERVER['HTTP_X_API_KEY'])) {
-    $apiKey = $_SERVER['HTTP_X_API_KEY'];
-} elseif (isset($_GET['api_key'])) {
+
+if (isset($_GET['api_key'])) {
     $apiKey = $_GET['api_key'];
+} else {
+    $apiKey = generateApiKey();
 }
+
 header('API-KEY: ' . $apiKey);
 
 //Json file
-$url = 'https://yoururl.com/api.json';
+$url = 'https://bit.codeaxe.in/AB454244s5d4Dfds54.json?api_key=' . urlencode($apiKey);
 $json_data = file_get_contents($url);
 $data = json_decode($json_data, true);
 
